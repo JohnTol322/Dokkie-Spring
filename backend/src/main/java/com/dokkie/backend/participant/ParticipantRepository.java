81a1035c3eb6929f8331def3469d06c58c4bf412
John Tol <johntol322@gmail.com>
2023-10-18 19:00:32 +0200

    Created Repository for Participant Entity

---
+++ backend/src/main/java/com/dokkie/backend/participant/ParticipantRepository.java
@@ -0,0 +1,5 @@
+package com.dokkie.backend.participant;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ParticipantRepository extends CrudRepository<Participant, Long> { }
