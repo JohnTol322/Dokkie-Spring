898b91ef3f8a7f9bff867e24fad6b3bd7ca6174f
John Tol <johntol322@gmail.com>
2023-10-19 16:30:59 +0200

    Changed User controller and service to return DTO instead entire object

--- backend/src/main/java/com/dokkie/backend/user/UserController.java
+++ backend/src/main/java/com/dokkie/backend/user/UserController.java
@@ -2,6 +2,8 @@ package com.dokkie.backend.user;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/api/v1/user")
 public class UserController {
@@ -18,12 +20,12 @@ public class UserController {
     }
 
     @GetMapping
-    public Iterable<User> listUsersAction() {
+    public List<UserDTO> listUsersAction() {
         return this.userService.listUsers();
     }
 
     @GetMapping(path = "/{id}")
-    public User getUserAction(@PathVariable("id") Long id) {
+    public UserDTO getUserAction(@PathVariable("id") Long id) {
         return this.userService.findUserById(id);
     }
 }
--- backend/src/main/java/com/dokkie/backend/user/UserService.java
+++ backend/src/main/java/com/dokkie/backend/user/UserService.java
@@ -2,6 +2,7 @@ package com.dokkie.backend.user;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,13 +19,17 @@ public class UserService {
         return this.userRepository.save(user);
     }
 
-    public User findUserById(Long id) {
-        Optional<User> value = this.userRepository.findById(id);
+    public UserDTO findUserById(Long id) {
+        Optional<User> users = this.userRepository.findById(id);
 
-        return value.orElse(null);
+        return users.map(UserDTO::new).orElse(null);
     }
 
-    public Iterable<User> listUsers() {
-        return this.userRepository.findAll();
+    public List<UserDTO> listUsers() {
+        Iterable<User> users = this.userRepository.findAll();
+        List<UserDTO> result = new ArrayList<>();
+        users.forEach(user -> result.add(new UserDTO(user)));
+
+        return result;
     }
 }
