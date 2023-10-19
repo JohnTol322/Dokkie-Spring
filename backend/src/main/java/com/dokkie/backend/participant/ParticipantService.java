4619553cffbc0714043dea793f25e3ec16e1327c
John Tol <johntol322@gmail.com>
2023-10-19 16:33:13 +0200

    Created Service for Participant Entity

---
+++ backend/src/main/java/com/dokkie/backend/participant/ParticipantService.java
@@ -0,0 +1,13 @@
+package com.dokkie.backend.participant;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParticipantService {
+
+    private final ParticipantRepository participantRepository;
+
+    public ParticipantService(ParticipantRepository participantRepository) {
+        this.participantRepository = participantRepository;
+    }
+}
